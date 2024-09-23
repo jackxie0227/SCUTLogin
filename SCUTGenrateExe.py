e9f5be4598cf87cd775fae6673901996929a9473
Jack Xie <jackxie3237@126.com>
2024-09-23 14:10:37 +0800

    "添加 py 转 exe 程序"

---
+++ SCUTGenrateExe.py
@@ -0,0 +1,10 @@
+import os
+import subprocess
+
+src_name = 'SCUTLogin.py'
+
+if os.path.isfile(src_name):
+    subprocess.run(['pyinstaller','--onefile',src_name])
+    print(f'{src_name} 已转为 exe 文件')
+else:
+    print(f'文件{src_name}不在同级目录下')
